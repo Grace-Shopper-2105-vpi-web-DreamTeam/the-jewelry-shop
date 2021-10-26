d1b89b660045e1028a01e092b8a8307ede9d6752
jsitinas94 <jsitinas94@gmail.com>
2021-10-26 19:45:42 -0400

    deployed backend to heroku

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export async function getSomething() {
   try {
     const { data } = await axios.get('/api');
+    console.log("you've hit the api")
     return data;
   } catch (error) {
     throw error;
