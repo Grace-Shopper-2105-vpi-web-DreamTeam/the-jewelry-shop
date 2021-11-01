0b1a4f050491483f5edcdbb3f16d3b7d5104046b
Mandy Levri <mandylevri@gmail.com>
2021-10-31 23:02:52 -0400

    cleaned up a little bit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,35 +9,28 @@ import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import Link from '@mui/material/Link';
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
-
 import { withRouter, useHistory } from 'react-router-dom';
 import {
     Logout
 } from "."
 
 const useStyles = makeStyles(theme => ({
-    
+
     menuButton: {
         marginRight: theme.spacing(2)
     },
-    
-  
-    
+
 }));
 
 const Navbar = ({ authenticated, setAuthenticated, setToken, admin }) => {
     let history = useHistory();
-    // const {history} = props;
     const [auth, setAuth] = React.useState(true);
     const [anchorEl, setAnchorEl] = React.useState(null);
     const classes = useStyles();
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
-
-
 
     const handleMenu = (event) => {
         setAnchorEl(event.currentTarget);
@@ -56,17 +49,6 @@ const Navbar = ({ authenticated, setAuthenticated, setToken, admin }) => {
     const handleLogoClick = () => {
         history.push("/")
     };
-
-
-
-    // const handleLogout = () => {
-    // <Logout
-    // setAuthenticated={setAuthenticated}
-    // setToken ={setToken}/>
-    // }
-
-
-
 
     return (
         <Box sx={{ flexGrow: 1 }}>
@@ -98,27 +80,27 @@ const Navbar = ({ authenticated, setAuthenticated, setToken, admin }) => {
                         onClose={() => setAnchorEl(null)}
                     >
                         <MenuItem onClick={() => handleMenuClick('/')}>Home</MenuItem>
-                        {!authenticated?(
-                        <MenuItem onClick={() => handleMenuClick('/login')}>Login/Register</MenuItem>): null}
+                        {!authenticated ? (
+                            <MenuItem onClick={() => handleMenuClick('/login')}>Login/Register</MenuItem>) : null}
                         {admin ? (
-                        <MenuItem onClick={() => handleMenuClick('/admin')}>Admin</MenuItem>): null}
+                            <MenuItem onClick={() => handleMenuClick('/admin')}>Admin</MenuItem>) : null}
                         {authenticated ? (
-                        <MenuItem >
-                            <Logout
-                                setAuthenticated={setAuthenticated}
-                                setToken={setToken} />
-                        </MenuItem>) : null
-                    }
-                        
+                            <MenuItem >
+                                <Logout
+                                    setAuthenticated={setAuthenticated}
+                                    setToken={setToken} />
+                            </MenuItem>) : null
+                        }
+
                     </Menu>
-                  {isMobile ? (    <Typography  variant="h6" component="div" sx={{ flexGrow: 1}} >
+                    {isMobile ? (<Typography variant="h6" component="div" sx={{ flexGrow: 1 }} >
                         <img src="https://thejewelryshop.s3.us-east-2.amazonaws.com/Logo/Loops%26Strings+No+background+Pink+(100+x+50+px)+(1).png" onClick={handleLogoClick} />
                     </Typography>
-                    ):(
-                    <Typography  variant="h6" component="div" sx={{ flexGrow: 1}} >
-                        <img src="https://thejewelryshop.s3.us-east-2.amazonaws.com/Logo/Loops%26Strings+No+background+Pink+(200+x+100+px)+.png" onClick={handleLogoClick} />
-                    </Typography>)}
-                
+                    ) : (
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} >
+                            <img src="https://thejewelryshop.s3.us-east-2.amazonaws.com/Logo/Loops%26Strings+No+background+Pink+(200+x+100+px)+.png" onClick={handleLogoClick} />
+                        </Typography>)}
+
 
                     {authenticated ? (
                         <div className={classes.menuButton}>
@@ -143,14 +125,6 @@ const Navbar = ({ authenticated, setAuthenticated, setToken, admin }) => {
                             <ShoppingCartIcon onClick={handleCartClick} />
                         </IconButton>
                     </div>
-                    {/* {authenticated ? (
-                        <Typography >
-                            <Logout
-                                setAuthenticated={setAuthenticated}
-                                setToken={setToken} />
-                        </Typography>) : null
-                    } */}
-
                 </Toolbar>
             </AppBar>
         </Box>
