958aa83edb92702238f6d5a19eb94b2e5edcc7b4
Jacob Merkel <soccerjacobjm17@gmail.com>
2021-11-05 13:52:18 -0400

    homepage with random featured items

---
+++ src/components/HomePage.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react';
+
+import { default as ProductCard } from "./ProductCards";
+import { default as CategoryBanner } from "./CategoryBanner"
+
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import { getAllProducts } from '../api';
+import { getProductById } from '../../db/products';
+
+export default function Products({category, setCategory}) {
+    const [products, setProducts] = useState([]);
+
+    console.log("the products are", products)
+
+    useEffect(() => {
+        const getResult = async () => {
+             const results = await getAllProducts()
+             setProducts(results)
+        }
+        getResult();
+    }, []);
+  
+function productRandomizor(min, max) {
+    return Math.random() * (max- min) + min;
+}
+
+function getSixRandomProducts() {
+    randomProductArray = []
+    for(i= 0; i < 7; i++) {
+        productId = productRandomizor(1, 105);
+        randomProduct = getProductById(productId)
+        randomProductArray.push(randomProduct)
+    }
+
+getSixRandomProducts();
+
+}
+
+    return (
+        <Container>
+            <h2>Home Page </h2> 
+            <CategoryBanner 
+                category={category}
+                setCategory={setCategory}
+            /> 
+            {/* panel with clickable images of categories go here */}
+            <h1
+                style={{
+                    marginBottom: "10px",
+                    fontFamily: "sans-serif"
+                }}
+                >
+                    Top Sellers
+                </h1>
+            <div
+                style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                marginBottom: "50px",
+                }}
+            >
+                <Grid container spacing={3} alignItems="stretch">
+                    {randomProductArray.map((product) => (
+                        <Grid
+                            item
+                            xs={12}
+                            md={6}
+                            lg={4}
+                            key={product.id}
+                            style={{ height: "100%" }}
+                        >
+                            <ProductCard
+                                product={product}
+                            />
+                        </Grid>
+                    ))}
+            </Grid>        
+            </div>
+        </Container>
+    )
+}
