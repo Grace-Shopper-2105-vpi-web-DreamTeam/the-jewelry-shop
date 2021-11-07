cdd6548f9779a3b1d12b9200f99da15bf700a709
Mandy Levri <mandylevri@gmail.com>
2021-11-06 20:24:27 -0400

    added a banner to home

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,9 +7,10 @@ import { default as CategoryBanner } from "./CategoryBanner"
 
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
-import { getAllProducts} from '../api';
+import { getAllProducts } from '../api';
+import featured from "../imgs/featured.png";
 
-export default function HomePage({allProducts}) {
+export default function HomePage({ allProducts }) {
     const [featuredProducts, setFeaturedProducts] = useState([]);
 
     const inputRef = useRef();
@@ -18,63 +19,68 @@ export default function HomePage({allProducts}) {
 
         function getSixRandomProducts() {
             let randomProductsArray = allProducts.sort(() => .5 - Math.random()).slice(0, 6)
-            setFeaturedProducts(randomProductsArray);  
+            setFeaturedProducts(randomProductsArray);
         }
-            getSixRandomProducts();
-        
+        getSixRandomProducts();
+
     }, [allProducts]);
 
     return (
-        <Container>
-           <h1
-                style={{
-                    marginTop: "10px",
-                    marginBottom: "10px",
-                    fontFamily: "sans-serif",
-                    textAlign: "center"
-                }}
-                >
-                   Welcome to Loops <span>&#38;</span>  Strings! 
-                   <br />
-                </h1>
-            <h2
-                style={{
-                    marginTop: "10px",
-                    marginBottom: "10px",
-                    fontFamily: "sans-serif",
-                    textAlign: "center"
-                }}
-                >
-                   View a selection of our Top Sellers Below
-                   < br />
-                   <Link style={{textDecoration: "none"}} to="/jewelry"> Click Here to Shop All our Jewelry</Link>
-                </h2>
-            <div
-                style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column",
-                marginBottom: "50px",
-                }}
-            >
-                <Grid container spacing={3} alignItems="stretch">
-                    {featuredProducts.map((product) => (
-                        <Grid
-                            item
-                            xs={12}
-                            md={6}
-                            lg={4}
-                            key={product.id}
-                            style={{ height: "100%" }}
-                        >
-                            <ProductCard
-                                product={product}
-                            />
-                        </Grid>
-                    ))}
-            </Grid>        
+        <div >
+            <div>
+            <Link to="/jewelry"><img className="banner" src={featured} alt="site banner"></img></Link>
             </div>
-        </Container>
+            <Container>
+                <h1
+                    style={{
+                        marginTop: "10px",
+                        marginBottom: "10px",
+                        fontFamily: "sans-serif",
+                        textAlign: "center"
+                    }}
+                >
+                    Top Sellers!
+                    <br />
+                </h1>
+                {/* <h2
+                    style={{
+                        marginTop: "10px",
+                        marginBottom: "10px",
+                        fontFamily: "sans-serif",
+                        textAlign: "center"
+                    }}
+                >
+                    View a selection of our Top Sellers Below
+                    < br />
+                    <Link style={{ textDecoration: "none" }} to="/jewelry"> Click Here to Shop All our Jewelry</Link>
+                </h2> */}
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        flexDirection: "column",
+                        marginBottom: "50px",
+                    }}
+                >
+                    <Grid container spacing={3} alignItems="stretch">
+                        {featuredProducts.map((product) => (
+                            <Grid
+                                item
+                                xs={12}
+                                md={6}
+                                lg={4}
+                                key={product.id}
+                                style={{ height: "100%" }}
+                            >
+                                <ProductCard
+                                    product={product}
+                                />
+                            </Grid>
+                        ))}
+                    </Grid>
+                </div>
+            </Container>
+        </div>
     )
 }
